f01c35acbf54960a192fb09d84d863953c0d4f3c
ljh95 <ilegwmam@gmail.com>
2021-04-10 14:58:25 +0900

    빨간 점 까지 보이게 하기!

---
+++ app.js
@@ -0,0 +1,39 @@
+import { Wave } from './wave.js';
+
+class App {
+  constructor() {
+    this.canvas = document.createElement('canvas');// canvas create
+    this.ctx = this.canvas.getContext('2d');
+    document.body.appendChild(this.canvas);
+
+    this.wave = new Wave();
+
+    window.addEventListener('resize', this.resize.bind(this), false); //resize evenet
+    this.resize();
+
+    requestAnimationFrame(this.animate.bind(this)); // anmation start
+  }
+
+  resize() {
+    this.stageWidth = document.body.clientWidth;
+    this.stageHeight = document.body.clientHeight;
+
+    this.canvas.width = this.stageWidth * 2;
+    this.canvas.height = this.stageHeight * 2;
+    this.ctx.scale(2, 2); // canvas double size for letina display
+
+    this.wave.resize(this.stageWidth, this.stageHeight);
+  }
+
+  animate(t) {
+    this.ctx.clearRect(0, 0, this.stageWidth, this.stageHeight); // canvas clear
+
+    this.wave.draw(this.ctx);
+
+    requestAnimationFrame(this.animate.bind(this));
+  }
+}
+
+window.onload = () => { // window load create App
+  new App();
+}
